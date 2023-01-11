d6ee59cb40c6b9da399f9123c262442e2ebbb179
Lysander Myrddin-Evans <myrddin-evans/.l@etoncollege.org.uk>
2023-01-11 19:57:51 +0000

    Initial Commit

---
+++ CarCustomiser/CarCustomiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCustomiserApp.swift
+//  CarCustomiser
+//
+//  Created by Lysander Myrddin-Evans on 11/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CarCustomiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
