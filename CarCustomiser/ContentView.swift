f34414eebae52218821e8be892885404045c336e
Lysander Myrddin-Evans <myrddin-evans/.l@etoncollege.org.uk>
2023-01-12 13:08:19 +0000

    Car customiser part 2

--- CarCustomiser/ContentView.swift
+++ CarCustomiser/ContentView.swift
@@ -8,11 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
-    let car = Car(make: "Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5)
+    @State private var starterCars = StarterCars()
+    @State private var selectedCar: Int = 0
+    @State private var exhaustPackage = false
+    @State private var tiresPackage = false
+    @State private var gearboxPackage = false
+    @State private var spoiler = false
     
     var body: some View {
-        Text("\(car.displayStats())")
-        .padding()
+        let exhaustPackageBinding = Binding<Bool> (
+            get: { self.exhaustPackage },
+            set: { newValue in
+                self.exhaustPackage = newValue
+                if newValue == true {
+                    starterCars.cars[selectedCar].topSpeed += 5
+                } else {
+                    starterCars.cars[selectedCar].topSpeed -= 5
+                }
+            }
+        )
+        let tiresPackageBinding = Binding<Bool> (
+            get: { self.tiresPackage },
+            set: { newValue in
+                self.tiresPackage = newValue
+                if newValue == true {
+                    starterCars.cars[selectedCar].handling += 2
+                } else {
+                    starterCars.cars[selectedCar].handling -= 2
+                }
+            }
+        )
+        let gearboxPackageBinding = Binding<Bool> (
+            get: { self.gearboxPackage },
+            set: { newValue in
+                self.gearboxPackage = newValue
+                if newValue == true {
+                    starterCars.cars[selectedCar].acceleration -= 1
+                } else {
+                    starterCars.cars[selectedCar].acceleration += 1
+                }
+            }
+        )
+        let spoilerBinding = Binding<Bool> (
+            get: { self.spoiler },
+            set: { newValue in
+                self.spoiler = newValue
+                if newValue == true {
+                    starterCars.cars[selectedCar].topSpeed += 5
+                    starterCars.cars[selectedCar].handling += 1
+                } else {
+                    starterCars.cars[selectedCar].topSpeed -= 5
+                    starterCars.cars[selectedCar].handling -= 1
+                }
+            }
+        )
+        
+        Form {
+            VStack(alignment: .leading, spacing: 20) {
+                Text("\(starterCars.cars[selectedCar].displayStats())")
+                Button("Next Car", action: {
+                    if selectedCar == self.starterCars.cars.count - 1 {
+                        selectedCar = 0
+                    } else {
+                        selectedCar += 1
+                    }
+                    exhaustPackage = false
+                    tiresPackage = false
+                    gearboxPackage = false
+                    spoiler = false
+                })
+            }
+            Section {
+                Toggle("Exhaust Package", isOn: exhaustPackageBinding)
+                Toggle("Tires package", isOn: tiresPackageBinding)
+                Toggle("Gearbox package", isOn: gearboxPackageBinding)
+                Toggle("Spoiler", isOn: spoilerBinding)
+            }
+        }
     }
 }
 
---
+++ CarCustomiser/StarterCars.swift
@@ -0,0 +1,12 @@
+//
+//  StarterCars.swift
+//  CarCustomiser
+//
+//  Created by Lysander Myrddin-Evans on 12/01/2023.
+//
+
+import Foundation
+
+struct StarterCars {
+    var cars = [Car(make: "Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5), Car(make: "Audi", model: "A4", topSpeed: 133, acceleration: 5.7, handling: 3), Car(make: "Aston Martin", model: "DB5", topSpeed: 99, acceleration: 4.5, handling: 6), Car(make: "Dacia", model: "Duster", topSpeed: 84, acceleration: 9, handling: 8)]
+}
